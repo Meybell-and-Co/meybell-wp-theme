f137d4841684d0d82f6b46be1a1a9853fde60df1
Mark Meybell <mark@meybell.com>
2026-07-30 08:07:21 -0500

    Updated index.php for Local testing

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * The main template file.
+ *
+ * @package Meybell_Framework
+ */
+
+get_header();
+?>
+
+<main class="mnco-site">
+
+	<h1>Hello, Meybell.</h1>
+
+	<p>Your framework is alive.</p>
+
+</main>
+
+<?php
+get_footer();
