91cc13ba18b23b3dd2a6e1e2ed23854e462eeb16
Meybell-and-Co <mark@meybell.com>
2026-09-12 07:50:37 -0500

    Fix theme bootstrap demo dependency

--- functions.php
+++ functions.php
@@ -16,4 +16,3 @@ require_once get_template_directory() . '/inc/enqueue.php';
 require_once get_template_directory() . '/inc/navigation.php';
 require_once get_template_directory() . '/inc/editor.php';
 require_once get_template_directory() . '/inc/template-functions.php';
-require_once get_template_directory() . '/inc/demo-mode.php';
