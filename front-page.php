c88830791a1316cb5724ee21632c32cbaa3df2bf
Meybell-and-Co <mark@meybell.com>
2026-09-12 08:59:57 -0500

    Add Meybell holding page template

---
+++ front-page.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Temporary Meybell & Co. front page.
+ *
+ * @package Meybell_Framework
+ */
+
+get_header();
+?>
+
+<main class="mnco-holding-page">
+	<div class="mnco-holding-page__inner">
+		<img
+			class="mnco-holding-page__logo"
+			src="https://assets.meybell.com/brand/logos/mnco-logo-full-color-cream.svg"
+			alt="Meybell & Co."
+		>
+
+		<div class="mnco-holding-page__content">
+			<h1>We're making some changes.</h1>
+
+			<p class="mnco-holding-page__intro">
+				Our new home is taking shape, but we're still here for you.
+			</p>
+
+			<p class="mnco-holding-page__positioning">
+				We turn complicated work into smart, useful, gorgeous systems.
+			</p>
+
+			<a class="mnco-holding-page__contact" href="mailto:hello@meybell.com">
+				Start a conversation
+			</a>
+		</div>
+	</div>
+</main>
+
+<?php
+get_footer();
